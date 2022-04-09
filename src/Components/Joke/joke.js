45d7db16b812722bc9b132c03277a869dccca09e
Mhmd Houmani <mhoumani742@gmail.com>
2022-04-09 16:47:48 +0300

    edit Joke component

--- src/Components/Joke/joke.js
+++ src/Components/Joke/joke.js
@@ -6,7 +6,13 @@ const Joke = (props) => {
   return (
     <div className="joke-container">
       {/* Check if there is a setup before display it to the screen */}
-      {props.setup && <h3>Setup: {props.setup}</h3>}
+      <h3
+        style={{ display: props.setup ? "block" : "none", color: "lightblue" }}
+      >
+        Setup: {props.setup}
+      </h3>
+      {/* Or this way */}
+      {/* {props.setup && <h3>Setup: {props.setup}</h3>} */}
       <p>Punchline: {props.punchline}</p>
     </div>
   );
